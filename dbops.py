9e59b2f293a3ce1810fcfdca753998e1b6ed07ed
stuianna <stuian@protonmail.com>
2020-04-12 22:40:09 +0300

    Added tests for initialising, adding and removing tables. Some formatting for PEP8

--- dbops.py
+++ dbops.py
@@ -5,12 +5,12 @@ Author Stuart Ianna
 """
 
 import sqlite3 as sq
-import sys
 import pandas as pd
 import logging
 import os
 
 log = logging.getLogger(__name__)
+
 
 class DBOps():
     """Class for working with a single database
@@ -23,7 +23,8 @@ class DBOps():
     database = DBOps('database.db')
 
     # Add a table to the database
-    database.createTableIfNotExist('my_table','column_1 NUMERIC, column_2 TEXT, column_3 TEXT')
+    database.createTableIfNotExist('my_table','column_1 NUMERIC, \
+        column_2 TEXT, column_3 TEXT')
 
     # Get all the tables in the database
     database.getTableNames()
@@ -51,49 +52,65 @@ class DBOps():
         - table2Df(tableName) - return the complete table as a dataframe.
         - getLastTimeEntry(table) - get the latest time entry in a table. Note:
             This assumes the table contains a column labeled 'timestamp'
-        - getRowRange(table,column,minimum,maximum) Returns a dataframe of entries
-            whose column matched the specific values.
-        - getLastRows(table,maximum) - returns up to the maximum rows as a dataframe.
-            This assumes there is a column named timestamp.
+        - getRowRange(table,column,minimum,maximum) Returns a dataframe of
+            entries whose column matched the specific values.
+        - getLastRows(table,maximum) - returns up to the maximum rows as a
+            dataframe. This assumes there is a column named timestamp.
         - removeRowRange(table,column,minimum,maximum) - remove rows from the
             table where the column values match.
-        - append(table,values) - add an entry to the database, value are passed as
-            a list. Note: the number of items in the list must match the number
-            of columns.
-
+        - append(table,values) - add an entry to the database, value are passed
+            as a list. Note: the number of items in the list must match the
+            number of columns.
     """
 
-    def __init__(self,db_name):
+    def __init__(self, db_name):
 
         self.__dbName = db_name
+        self.con = None
         self.createDatabase()
 
     def createDatabase(self):
         try:
             self.con = sq.connect(self.__dbName)
-        except:
-            log.error('Cannot connect to database {}'.format(self.__dbName))
+        except Exception as e:
+            log.error('Cannot connect to database {}, raised exception {}.'.format(self.__dbName, e))
 
     def exists(self):
-        if os.path.exists(self.__dbName):
+        if os.path.exists(self.__dbName) and self.con is not None:
             return True
         return False
 
+    def __checkDatabaseIsInitialised(self):
+        if self.con is None:
+            logging.error("Trying to operate on a database which doesn't exist \
+                          or hasn't been initialised.")
+            return False
+        return True
 
-    def createTableIfNotExist(self,tableName,columns):
+    def createTableIfNotExist(self, tableName, columns):
+
+        if self.__checkDatabaseIsInitialised() is False:
+            return []
 
         cur = self.con.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS {}({})".format(tableName,columns))
+        try:
+            cur.execute("CREATE TABLE IF NOT EXISTS {}({})".format(tableName, columns))
+        except sq.OperationalError:
+            log.error("Trying to create table with illegle name/s table: {} column: {}".format(tableName, columns))
+            return []
         self.con.commit()
         return self.getColumnNames(tableName)
 
-    def removeTable(self,tableName):
+    def removeTable(self, tableName):
+
+        if self.__checkDatabaseIsInitialised() is False:
+            return False
 
         cur = self.con.cursor()
         try:
             cur.execute("DROP TABLE {}".format(tableName))
-        except:
-            log.error('Cannot remove table: {}. Does not exist'.format(tableName))
+        except Exception as e:
+            log.error('Cannot remove table: {}. Exception {}.'.format(tableName, e))
             return False
         self.con.commit()
 
@@ -101,21 +118,29 @@ class DBOps():
 
     def getTableNames(self):
 
+        if self.__checkDatabaseIsInitialised() is False:
+            return []
+
         cur = self.con.cursor()
         finalList = []
-        tupleList = cur.execute("SELECT name FROM sqlite_master WHERE type = 'table'").fetchall()
+        tupleList = cur.execute(
+            "SELECT name FROM sqlite_master WHERE type = 'table'").fetchall()
         for l in tupleList:
             finalList.append(l[0])
         return finalList
 
-    def getColumnNames(self,table):
+    def getColumnNames(self, table):
+
+        if self.__checkDatabaseIsInitialised() is False:
+            return []
 
         cur = self.con.cursor()
         try:
-            cur.execute("PRAGMA table_info({})".format(table));
-        except:
-            #Table didn't exist
-            return None
+            cur.execute("PRAGMA table_info({})".format(table))
+        except Exception as e:
+            logging.error("Exception {} when trying to get column \
+                            names for table {}".format(e, table))
+            return []
 
         fullList = cur.fetchall()
         nameList = []
@@ -131,29 +156,35 @@ class DBOps():
         try:
             for row in cur.execute("SELECT * FROM {}".format(table)):
                 print(row)
-        except:
-            #Table didn't exist
+        except Exception as e:
+            logging.error("Exception {} when trying to print table\
+                            {}".format(e, table))
             return None
 
-    def table2Df(self,table):
+    def table2Df(self, table):
 
         cur = self.con.cursor()
         try:
             cur.execute("SELECT * FROM {}".format(table))
-        except:
-            #Table didn't exist
+        except Exception as e:
+            logging.error("Exception {} when trying to return table\
+                            {} as a Dataframe".format(e, table))
             return None
-        df = pd.DataFrame(cur.fetchall(),columns=self.getColumnNames(table))
+        df = pd.DataFrame(cur.fetchall(), columns=self.getColumnNames(table))
         return df
 
-    #This assumes that the table contains a column named "timestamp"
-    def getLastTimeEntry(self,table):
+    # This assumes that the table contains a column named "timestamp"
+    def getLastTimeEntry(self, table):
 
         cur = self.con.cursor()
         try:
-            entry = cur.execute("SELECT * FROM {} ORDER BY timestamp DESC LIMIT 1".format(table))
-        except:
-            log.error('Cannot get last time entry from {}. Does the table have a timestamp column?'.format(table))
+            entry = cur.execute(
+                "SELECT * FROM {} ORDER BY timestamp DESC LIMIT 1".format(
+                    table))
+        except Exception as e:
+            log.error('Cannot get last time entry from {}.\
+                      Does the table have a timestamp column? \
+                      Exception {}'.format(table, e))
             return None
 
         lastEntry = entry.fetchall()
@@ -163,59 +194,75 @@ class DBOps():
             lastEntry = 0
             return lastEntry
 
-    def getRowRange(self,table,column,minimum,maximum):
+    def getRowRange(self, table, column, minimum, maximum):
 
         cur = self.con.cursor()
         try:
-            return pd.DataFrame(cur.execute("SELECT * FROM {} WHERE {} BETWEEN {} AND {}".format(table,
-                column,minimum,maximum)).fetchall(),columns=self.getColumnNames(table))
-        except:
-            log.error('Cannot query rows from {}. Requested column: {}. Availabe: {}?'.format(table,
-                column,self.getColumnNames))
+            return pd.DataFrame(cur.execute(
+                "SELECT * FROM {} WHERE {} BETWEEN {} AND {}".format(
+                    table,
+                    column,
+                    minimum,
+                    maximum)).fetchall(), columns=self.getColumnNames(table))
+        except Exception as e:
+            log.error('Cannot query rows from {}. \
+                      Requested column: {}.\
+                      Availabe: {}?, excpetion {}'.format(
+                          table, column, self.getColumnNames, e))
             return None
 
-
-    def getRow(self,table,column,query):
+    def getRow(self, table, column, query):
 
         cur = self.con.cursor()
         try:
             if type(query) is str:
-                return pd.DataFrame(cur.execute("SELECT * FROM {} WHERE {} LIKE '{}'".format(table,
-                    column,query)).fetchall(),columns=self.getColumnNames(table))
+                return pd.DataFrame(cur.execute(
+                    "SELECT * FROM {} WHERE {} LIKE '{}'".format(
+                        table, column, query)).fetchall(),
+                                    columns=self.getColumnNames(table))
             else:
-                return pd.DataFrame(cur.execute("SELECT * FROM {} WHERE {} = {}".format(table,
-                    column,query)).fetchall(),columns=self.getColumnNames(table))
-        except:
-            log.error('Cannot query rows from {}. Requested column: {}. Availabe: {}?'.format(table,
-                column,self.getColumnNames))
+                return pd.DataFrame(cur.execute(
+                    "SELECT * FROM {} WHERE {} = {}".format(
+                        table, column, query)).fetchall(),
+                                    columns=self.getColumnNames(table))
+        except Exception as e:
+            log.error('Cannot query rows from {}. \
+                      Requested column: {}. Availabe: {}?. \
+                      Exception {}'.format(
+                          table, column, self.getColumnNames, e))
 
-    def getLastRows(self,table,maximum):
+    def getLastRows(self, table, maximum):
 
         cur = self.con.cursor()
         try:
-            return pd.DataFrame(cur.execute("SELECT * FROM {} ORDER BY timestamp DESC LIMIT {}".format(table,
-                maximum)).fetchall(),columns=self.getColumnNames(table))
-        except:
-            #Table didn't exist
-            log.error('Could not get last rows from {}, does the table have a timestamp column?'.format(table))
+            return pd.DataFrame(cur.execute(
+                "SELECT * FROM {} ORDER BY timestamp DESC LIMIT {}".format(
+                    table, maximum)).fetchall(), columns=self.getColumnNames(table))
+        except Exception as e:
+            log.error('Could not get last rows from {}. \
+                      Does the table have a timestamp column? \
+                      Exception {}'.format(
+                          table, e))
             return None
 
-    def removeRowRange(self,table,column,minimum,maximum):
+    def removeRowRange(self, table, column, minimum, maximum):
 
         cur = self.con.cursor()
         try:
-            cur.execute("DELETE FROM {} WHERE {} BETWEEN {} AND {}".format(table,
-                column,minimum,maximum))
-        except:
-            log.error('Cannot remove rows from {}. Requested column: {}. Availabe: {}?'.format(table,
-                column,self.getColumnNames))
-            #Table didn't exist
+            cur.execute(
+                "DELETE FROM {} WHERE {} BETWEEN {} AND {}".format(
+                    table, column, minimum, maximum))
+        except Exception as e:
+            log.error('Cannot remove rows from {}. \
+                      Requested column: {}. \
+                      Availabe: {}?. Exception {}'.format(
+                          table, column, self.getColumnNames, e))
             return None
         self.con.commit()
 
-    #This is implemented in a silly way.
-    #Definitely a better way to do this.
-    def append(self,table,values):
+    # This is implemented in a silly way.
+    # Definitely a better way to do this.
+    def append(self, table, values):
 
         cur = self.con.cursor()
         if type(values) is not list:
--- tests/test_dbops.py
+++ tests/test_dbops.py
@@ -1,20 +1,213 @@
 import unittest
 import logging
+import os
 from dbops import DBOps
+
+test_db_name = "test_db.sql"
+
+logging.disable(logging.CRITICAL)
 
 
 class DBOpsTesting(unittest.TestCase):
 
     def setUp(self):
+        self.db = DBOps(test_db_name)
         pass
 
-    def teadDown(self):
-        pass
+    def tearDown(self):
+        try:
+            os.remove(test_db_name)
+        except Exception as e:
+            print("No database file was removed, exception {}".format(e))
 
-    def test_creating_database_and_test_if_exits_good_parameters(self):
-        self.assertIs(True, True)
+    def test_creating_database_and_test_if_exits_good_name(self):
+        exists = self.db.exists()
+        self.assertIs(exists, True)
 
+    def test_creating_database_and_test_if_exits_bad_name_database_already_exists(self):
+        db = DBOps('tests')  # Same name as tests directory
+        exists = db.exists()
+        self.assertIs(exists, False)
+
+    def test_creating_database_and_test_if_exits_bad_permissions(self):
+        db = DBOps('/usr/test_db.sql')
+        exists = db.exists()
+        self.assertIs(exists, False)
+
+    def test_creating_database_and_test_if_exits_in_nested_directory(self):
+        db = DBOps('non/existant/directory/db_name.sql')
+        exists = db.exists()
+        self.assertIs(exists, False)
+
+    def test_creating_a_new_table_with_columns_no_datatypes_string_column_input_good_format(self):
+
+        tablename = 'test_table'
+        columns = "column1, column2, column3"
+        self.db.createTableIfNotExist(tablename, columns)
+
+        # Check for correct table name
+        tableTuples = self.db.con.cursor().execute("SELECT name FROM sqlite_master WHERE type = 'table'").fetchall()
+        self.assertEqual(len(tableTuples), 1)
+        self.assertEqual(tableTuples[0][0], tablename)
+
+        # Check for correct column names in table
+        tableColumns = self.db.con.cursor().execute("PRAGMA table_info({})".format(tablename)).fetchall()
+        expectedColumns = columns.split(', ')
+        self.assertEqual(len(tableColumns), len(expectedColumns))
+        for i in range(len(expectedColumns)):
+            self.assertEqual(tableColumns[i][1], expectedColumns[i])
+
+        return tablename, columns
+
+    def test_creating_a_new_table_which_already_exists_doesnt_overwrite_it(self):
+
+        # Create a table
+        tablename, columns = self.test_creating_a_new_table_with_columns_no_datatypes_string_column_input_good_format()
+        new_columns = "newColumn1"
+
+        # Recreate same table with different names
+        self.db.createTableIfNotExist(tablename, new_columns)
+
+        # Check for correct table name
+        tableTuples = self.db.con.cursor().execute("SELECT name FROM sqlite_master WHERE type = 'table'").fetchall()
+        self.assertEqual(len(tableTuples), 1)
+        self.assertEqual(tableTuples[0][0], tablename)
+
+        # Check for correct column names in table
+        tableColumns = self.db.con.cursor().execute("PRAGMA table_info({})".format(tablename)).fetchall()
+        expectedColumns = columns.split(', ')
+        self.assertEqual(len(tableColumns), len(expectedColumns))
+        for i in range(len(expectedColumns)):
+            self.assertEqual(tableColumns[i][1], expectedColumns[i])
+
+    def test_creating_a_new_table_with_columns_no_datatypes_string_column_input_bad_format(self):
+
+        tablename = 'test_table'
+        columns = "columns1, CREATE, column2"
+        self.db.createTableIfNotExist(tablename, columns)
+
+        tableColumns = self.db.con.cursor().execute("PRAGMA table_info({})".format(tablename)).fetchall()
+        self.assertEqual(len(tableColumns), 0)  # No columns should be made
+
+    def test_try_to_make_table_if_database_is_not_setup(self):
+
+        db = DBOps('/usr/test_db.sql')   # Bad db name
+        tablename = 'test_table'
+        columns = "column1, column2, column3"
+        createdColumns = db.createTableIfNotExist(tablename, columns)
+        self.assertEqual(len(createdColumns), 0)
+
+    def test_create_table_with_bad_table_name(self):
+
+        tablename = '123test.table'
+        columns = "column1, column2, column3"
+        createdColumns = self.db.createTableIfNotExist(tablename, columns)
+        self.assertEqual(len(createdColumns), 0)
+
+    def test_getting_table_names_if_one_table_exists(self):
+
+        newtablename = 'test_table'
+        columns = "columns1, columns2, column2"
+        self.db.createTableIfNotExist(newtablename, columns)
+        tableNames = self.db.getTableNames()
+        self.assertEqual(tableNames[0], newtablename)
+
+    def test_getting_table_names_if_more_than_one_table_exists(self):
+
+        newtablename = 'test_table'
+        newtablename_2 = 'test_table_2'
+        tableList = [newtablename, newtablename_2]
+        columns = "columns1, columns2, column2"
+        self.db.createTableIfNotExist(newtablename, columns)
+        self.db.createTableIfNotExist(newtablename_2, columns)
+        tableNames = self.db.getTableNames()
+        self.assertEqual(tableNames, tableList)
+
+    def test_getting_table_names_if_no_tables_exist(self):
+
+        tableNames = self.db.getTableNames()
+        self.assertEqual(tableNames, [])
+
+    def test_getting_table_names_if_no_database_exists(self):
+
+        db = DBOps('/usr/test_db.sql')   # Bad db name
+        tableNames = db.getTableNames()
+        self.assertEqual(tableNames, [])
+
+    def test_getting_columns_names_with_one_column(self):
+
+        newtablename = 'test_table'
+        columns = "columns1"
+        expectedColumns = columns.split(', ')
+        self.db.createTableIfNotExist(newtablename, columns)
+        columnNames = self.db.getColumnNames(newtablename)
+        self.assertEqual(columnNames, expectedColumns)
+
+    def test_getting_columns_names_with_multiple_columns(self):
+
+        newtablename = 'test_table'
+        columns = "columns1, columns2, columns3"
+        expectedColumns = columns.split(', ')
+        self.db.createTableIfNotExist(newtablename, columns)
+        columnNames = self.db.getColumnNames(newtablename)
+        self.assertEqual(columnNames, expectedColumns)
+
+    def test_getting_columns_names_with_multiple_tables(self):
+
+        newtablename = 'test_table'
+        newtablename2 = 'test_table2'
+        columns = "columns1, columns2, columns3"
+        expectedColumns = columns.split(', ')
+        self.db.createTableIfNotExist(newtablename, columns)
+        self.db.createTableIfNotExist(newtablename2, columns)
+        columnNames = self.db.getColumnNames(newtablename2)
+        self.assertEqual(columnNames, expectedColumns)
+
+    def test_getting_columns_names_table_name_which_doesnt_exist(self):
+
+        newtablename = 'test_table'
+        columns = "columns1"
+        self.db.createTableIfNotExist(newtablename, columns)
+        columnNames = self.db.getColumnNames("not_a_table_name")
+        self.assertEqual([], columnNames)
+
+    def test_getting_columns_names_table_name_which_is_illeagle(self):
+
+        newtablename = 'test_table'
+        columns = "columns1"
+        self.db.createTableIfNotExist(newtablename, columns)
+        columnNames = self.db.getColumnNames("re.sd-s")
+        self.assertEqual([], columnNames)
+
+    def test_getting_columns_names_with_no_database_created(self):
+
+        db = DBOps('/usr/test_db.sql')   # Bad db name
+        columns = db.getColumnNames('some_table')
+        self.assertEqual(columns, [])
+
+    def test_removing_a_table_which_was_created_actually_removes_it(self):
+
+        newtablename = 'test_table'
+        columns = "columns1, columns2"
+        self.db.createTableIfNotExist(newtablename, columns)
+        removed = self.db.removeTable(newtablename)
+        self.assertIs(removed, True)
+        self.assertEqual([], self.db.getTableNames())
+
+    def test_removing_a_table_which_was_not_created_has_no_effect(self):
+
+        newtablename = 'test_table'
+        columns = "columns1, columns2"
+        self.db.createTableIfNotExist(newtablename, columns)
+        removed = self.db.removeTable("not_a_table_name")
+        self.assertIs(removed, False)
+        self.assertEqual([newtablename], self.db.getTableNames())
+
+    def test_removing_a_table_with_no_database_created_does_nothing(self):
+
+        db = DBOps('/usr/test_db.sql')   # Bad db name
+        removed = db.removeTable('some_name')
+        self.assertIs(removed, False)
 
 if __name__ == '__main__':
-    logging.disable(logging.CRITICAL)
     unittest.main()
