f1c475d27fe2dd568da0b679319492116c5b039a
stuianna <stuian@protonmail.com>
2020-04-18 23:32:37 +0300

    relase 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="db-ops",
-    version="0.0.1",
+    version="0.0.2",
     author="Stuart Ianna",
     author_email="stuian@protonmail.com",
     description="Python class helper for working with sqlite3 databases and Pandas dataframes.",
